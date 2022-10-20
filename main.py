3cac7be1131733a13d0597729c956ae998802e78
blank <3446953392@qq.com>
2022-10-20 15:14:47 +0800

    web大作业

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
+import zipfile
+import	threading
+def extractFile(zFile,password):
+    try:
+        zFile.extractall(pwd=password)
+        print("found password:",password)
+        return password
+    except :
+        pass
+def main():
+    zFile=zipfile.ZipFile('unzip.zip')
+    passFile = open('dictionary.txt')
+    for line in passFile.readlines():
+        password=line.strip('\n')
+        t = threading.Thread(target=extractFile, args=(zFile, password))
+        t.start()
 
 
-class count:
-
-    def __init__(self) :
-        self.intcount=[]
-        self.upstrcount=[]
-        self.lowstrcount=[]
-        self.othercount=[]
-
-    def number(self,a):
-        for i in a:
-            if i.isdigit():
-                self.intcount.append(i)
-            elif i.isupper():
-                self.upstrcount.append(i)
-            elif i.islower():
-                self.lowstrcount.append(i)
-            else:
-                self.othercount.append(i)
-    def intnumber(self):
-        return len(self.intcount)
-    def upstrnumber(self):
-        return len(self.upstrcount)
-    def lownumber(self):
-        return len(self.lowstrcount)
-    def othernumber(self):
-        return len(self.othercount)
-    def outputintnumber(self):
-        return tuple(self.intcount)
-    def outputupstrnumber(self):
-        return tuple(self.upstrcount)
-    def outputlownumber(self):
-        return tuple(self.lowstrcount)
-    def outputothernumber(self):
-        return tuple(self.othercount)
+if __name__ == '__main__':
+    main()
